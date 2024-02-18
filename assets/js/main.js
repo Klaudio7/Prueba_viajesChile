6d6503bc47d387c0bc449864fa1d8d2e8312dc1c
claudio <claudiosotojofre@gmail.com>
2024-02-18 01:12:54 -0300

    corregi la seleccion con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-let icono1 = $("icono1")
-let icono2 = $("icono2")
-let icono3 = $("icono3")
 
+$(document).ready(function () {
+
+    let iconoCambiar1 = $("#icono1")
+    let iconoCambiar2 = $("#icono2")
+    let iconoCambiar3 = $("#icono3")
+
+
+
+    iconoCambiar1.addClass("bg-info")
+
+
+
+})
