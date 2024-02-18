f7be81adfc826b01329dd4140f9973faf492d163
claudio <claudiosotojofre@gmail.com>
2024-02-18 00:53:02 -0300

    seleccione con jquery

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+let icono1 = $("icono1")
+let icono2 = $("icono2")
+let icono3 = $("icono3")
+
